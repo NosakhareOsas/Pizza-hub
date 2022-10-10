84d72f54de319592be242febc73308f328456349
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-10 13:30:14 +0100

    index action for restaurant created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,13 @@
 class RestaurantsController < ApplicationController
+    def index
+        render json: Restaurant.all, only: [:id, :name, :address], status: :ok
+    end
+
+    def show
+        # restaurant = Restaurant.find_by(id: params[:id])
+        # render json: restaurant.
+    end
+
+    def destroy
+    end
 end
