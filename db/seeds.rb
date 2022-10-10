a796a38c7d215d63f7c10410357bce88bc82fe61
Nosa Osagiede <osagiedenosakhare@gmail.com>
2022-10-10 12:42:11 +0100

    models and migrations generated

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "destroy all..."
+
+Restaurant.destroy_all
+Pizza.destroy_all
+RestaurantPizza.destroy_all
+puts "destroy done"
+
+puts "seeding in progress..."
+
+md = Restaurant.create!(name: "Macdonalds", address: "60 York Ave")
+cc = Restaurant.create!(name: "Crusty Crab", address: "7 New London")
+mb = Restaurant.create!(name: "Mr Biggs", address: "82 West Lagos")
+
+rr = Pizza.create!(name: "Rocket Red", ingredients: "Tomato, Pepper and Greens")
+pt = Pizza.create!(name: "Pineapple toppings", ingredients: "Fresh tomatoes and Pineapple slices")
+
+RestaurantPizza.create!(price: 25, restaurant: md, pizza: rr)
+RestaurantPizza.create!(price: 30, restaurant: md, pizza: pt)
+
+RestaurantPizza.create!(price: 23, restaurant: cc, pizza: rr)
+
+RestaurantPizza.create!(price: 26, restaurant: mb, pizza: rr)
+RestaurantPizza.create!(price: 28, restaurant: mb, pizza: pt)
+
+puts "seeding done!"
